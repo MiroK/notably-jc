425c9e6ea45c6417e4ea862052a4b62539f01cf2
mirok <miroslav.kuchta@gmail.com>
2019-06-23 17:56:05 +0200

    activation function scaling

--- src/tensorflow_ffnn.py
+++ src/tensorflow_ffnn.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
-
-def multilayer_perceptron(x, layer_dims):
+def multilayer_perceptron(x, layer_dims, scale=None):
     ''' o
        oo
       /ooo\
@@ -13,18 +12,22 @@ def multilayer_perceptron(x, layer_dims):
     Fully connected
     '''
     assert x.shape[1] == layer_dims[0]
-
+    
     ndofs = 0
     layer_i = x  # The previous one
     # Buld graph for all up to last hidden
     for dim_i, dim_o in zip(layer_dims[:-2], layer_dims[1:]):
         weights = tf.Variable(tf.truncated_normal([dim_i, dim_o], stddev=0.1))
         biases = tf.Variable(tf.constant(0.1, shape=[dim_o]))
-        ndofs += np.prod(weights.shape) + np.prod(biases.shape)
+
+        ndofs += np.prod(weights.shape) + np.prod(biases.shape) + 1
         
         layer_o = tf.add(tf.matmul(layer_i, weights), biases)
         # With ReLU activation
         layer_o = tf.nn.relu(layer_o)
+
+        # Common scaling for ReLU in each layer. FIXME: clipping
+        if scale is not None: layer_o = scale*tf.nn.relu(layer_o)
 
         layer_i = layer_o
 
@@ -35,6 +38,8 @@ def multilayer_perceptron(x, layer_dims):
     ndofs += np.prod(weights.shape) + np.prod(biases.shape)
     
     layer_o = tf.add(tf.matmul(layer_i, weights), biases)
+
+    if scale is not None: ndofs += 1
 
     return layer_o, ndofs
 
@@ -64,9 +69,11 @@ if __name__ == '__main__':
 
     x = tf.placeholder(tf.float32, [None, layer_dims[0]])
     y = tf.placeholder(tf.float32, [None, layer_dims[-1]])
+    # Activation function scaling
+    scale = tf.Variable(tf.constant(1.), constraint=lambda t: tf.clip_by_value(t, 1E-6, 100))
 
     # The net is now
-    NN, ndofs = multilayer_perceptron(x, layer_dims)
+    NN, ndofs = multilayer_perceptron(x, layer_dims, scale=scale)
 
     # The loss functional
     loss = tf.reduce_mean(tf.square(NN - y))
@@ -95,14 +102,14 @@ if __name__ == '__main__':
             y_test = target_foo(x_test)
 
             error = sess.run(loss, feed_dict={x: x_test, y: y_test})
-        
-            print 'At step %d error %g' % (step, error)
+            scale_ = sess.run(scale)
+            print('At step %d error %g, scale is %g' % (step, error, scale_))
 
     x_ = np.linspace(0, 1, 1000)
     y_ = [sess.run(NN, feed_dict={x: np.array([[xi]])}) for xi in x_]
     y_ = np.array(y_).flatten()
 
-    print 'Network size', ndofs
+    print('Network size', ndofs)
     
     plt.figure()
     plt.plot(x_, y_, label='num')
